6403147fc74c432e07449a3b0514bebe25c73d43
IGvidhyapathi <c.vidhyapathi1555@gmail.com>
2024-10-14 15:25:54 +0530

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,17 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
+export const metadata: Metadata = {
+  title: "Caption Craft AI",
+  description: "AI that Generate Socialmedia Captions",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
